9cce25e0b1a6433f0956d4a3419b11f103458f52
JoeButikofer <joebutikofer@gmail.com>
2015-10-08 22:42:30 +0200

    Add base buttons for scan and research on the action bar

--- bookList/src/main/java/mobop/booklist/app/BookListActivity.java
+++ bookList/src/main/java/mobop/booklist/app/BookListActivity.java
@@ -1,19 +1,18 @@
 package mobop.booklist.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class BookListActivity extends ActionBarActivity {
+public class BookListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,13 +26,15 @@ public class BookListActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                return true;
+            case R.id.action_scan:
+                //TODO QR code scan
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
